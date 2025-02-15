bf41da27dc828fe81f52efbc7123f98b366bf63d
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2025-02-15 13:06:36 +0000

    ansi parser crate

--- locatch_query/src/from.rs
+++ locatch_query/src/from.rs
@@ -1,5 +1,46 @@
 use crate::QueryBox;
 
+use ansi_parser::*;
+
 fn from_str(string: &str) -> QueryBox {
     todo!()
 }
+
+fn ansi_test() {
+    let mut iterator = "\"foo\\nbar\"".ansi_parse();
+
+    let token = iterator.next();
+
+    let token = match token {
+        Some(val) => val,
+        None => todo!(),
+    };
+    
+    match token {
+        Output::TextBlock(_) => todo!(),
+        Output::Escape(ansi_sequence) => todo!(),
+    }
+}
+
+// Expect value or group
+// Expect operator
+// Repeat
+
+// Value step entrance
+// Valid char:
+//  !
+//  "
+//  numeric (eventually)
+//  . (eventually, numeric)  
+
+// Value step iterator
+// Collect until '\' OR '"'
+// IF '\' the next step of iteration will ignore '"'
+// IF '"' EXIT
+
+// Operator step entrance
+// Expect either:
+//  &&
+//  ||
+// EXIT
+
