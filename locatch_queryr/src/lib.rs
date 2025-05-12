6a395c11f5a2da02a36e4adfbd5ad09ceca09826
orangutanrider <orangutanrider@gmail.com>
2025-05-12 14:26:25 +0100

    basic OR tests, passing
    Signed-off-by: orangutanrider <orangutanrider@gmail.com>

--- locatch_queryr/src/lib.rs
+++ locatch_queryr/src/lib.rs
@@ -295,8 +295,62 @@ mod test {
         assert_eq!(resolved, false, "false and false");
     }
 
-    // True OR (group = false)
-    // False OR (group = true)
+    #[test]
+    fn basic_or() {
+        let resolver = TestResolver;
+
+        // True or True
+        let statement: &str = stringify!("true" || "true");
+        let query = match QueryBox::try_from_str(statement) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Failed to create query (indicates issue with locatch_query)"),
+        };
+        let query = query.iter();
+        let resolved = match resolve_with(query, &resolver) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Unexpected resolver error"),
+        };
+        assert_eq!(resolved, true, "true or true");
+
+        // True or False
+        let statement: &str = stringify!("true" || "false");
+        let query = match QueryBox::try_from_str(statement) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Failed to create query (indicates issue with locatch_query)"),
+        };
+        let query = query.iter();
+        let resolved = match resolve_with(query, &resolver) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Unexpected resolver error"),
+        };
+        assert_eq!(resolved, true, "true or false");
+
+        // False or True
+        let statement: &str = stringify!("false" || "true");
+        let query = match QueryBox::try_from_str(statement) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Failed to create query (indicates issue with locatch_query)"),
+        };
+        let query = query.iter();
+        let resolved = match resolve_with(query, &resolver) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Unexpected resolver error"),
+        };
+        assert_eq!(resolved, true, "false or true");
+
+        // False or False
+        let statement: &str = stringify!("false" || "false");
+        let query = match QueryBox::try_from_str(statement) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Failed to create query (indicates issue with locatch_query)"),
+        };
+        let query = query.iter();
+        let resolved = match resolve_with(query, &resolver) {
+            Ok(ok) => ok,
+            Err(_) => panic!("Unexpected resolver error"),
+        };
+        assert_eq!(resolved, false, "false or false");
+    }
 
     // True AND (group = false)
     // False AND (group = true)
